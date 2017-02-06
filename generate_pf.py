bfcb69001c6fb73c840fe10e60ae809e921ed4c1
jhave <jhave2@gmail.com>
2017-02-06 17:48:50 -0500

    Add files via upload

---
+++ generate_pf.py
@@ -0,0 +1,93 @@
+###############################################################################
+# Language Modeling on Poetry Foundation Corpus
+#
+# This file generates new sentences sampled from the language model
+#
+###############################################################################
+
+import argparse
+import time
+import math
+
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+
+import data
+
+from random import randint
+from datetime import datetime
+started_datestring = "{0:%Y-%m-%dT%H-%M-%S}".format(datetime.now())
+words=''
+
+parser = argparse.ArgumentParser(description='PyTorch PF Language Model')
+
+# Model parameters.
+parser.add_argument('--data', type=str, default='./data/pf',
+                    help='location of the data corpus')
+parser.add_argument('--checkpoint', type=str, default='./model.pt',
+                    help='model checkpoint to use')
+parser.add_argument('--outf', type=str, default='GENERATED/'+started_datestring+'.txt',
+                    help='output file for generated text')
+parser.add_argument('--words', type=int, default='444',
+                    help='number of words to generate')
+parser.add_argument('--seed', type=int, default=randint(0,111111111),
+                    help='random seed')
+parser.add_argument('--cuda', action='store_true',
+                    help='use CUDA')
+parser.add_argument('--temperature', type=float, default=1.0,
+                    help='temperature - higher will increase diversity')
+parser.add_argument('--log-interval', type=int, default=100,
+                    help='reporting interval')
+args = parser.parse_args()
+
+# Set the random seed manually for reproducibility.
+torch.manual_seed(args.seed)
+if torch.cuda.is_available():
+    if not args.cuda:
+        print("WARNING: You have a CUDA device, so you should probably run with --cuda")
+    else:
+        torch.cuda.manual_seed(args.seed)
+
+if args.temperature < 1e-3:
+    parser.error("--temperature has to be greater or equal 1e-3")
+
+with open(args.checkpoint, 'rb') as f:
+    model = torch.load(f)
+
+if args.cuda:
+    model.cuda()
+else:
+    model.cpu()
+
+corpus = data.Corpus(args.data)
+ntokens = len(corpus.dictionary)
+hidden = model.init_hidden(1)
+input = Variable(torch.rand(1, 1).mul(ntokens).long(), volatile=True)
+if args.cuda:
+    input.data = input.data.cuda()
+
+with open(args.outf, 'w') as outf:
+    for i in range(args.words):
+        output, hidden = model(input, hidden)
+        word_weights = output.squeeze().data.div(args.temperature).exp().cpu()
+        word_idx = torch.multinomial(word_weights, 1)[0]
+        input.data.fill_(word_idx)
+        word = corpus.dictionary.idx2word[word_idx]
+
+        if word == '<eos>':
+            word = '\n'
+            
+        words+=word+" "
+            
+        outf.write(word + ('\n' if i % 20 == 19 else ' '))
+
+        if i % args.log_interval == 0:
+            print('| Generated {}/{} words'.format(i, args.words))
+
+    
+    print("SAVED "+str(args.words)+" word poem to "+args.outf)
+    
+    print('\n\n'+words)
+
+
